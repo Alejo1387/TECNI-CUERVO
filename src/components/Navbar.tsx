66dfa7af521895bbf111c7f3bbdbec44b8fd0d6d
Alejo1387 <trabajoalgoritmos02@gmail.com>
2026-05-15 15:33:54 -0500

    Changed link to logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="#" className="navbar-logo">
+        <a href="https://www.tiktok.com/@tecnicuervo?_r=1&_t=ZS-96OEYOnJr9R" target="_blank" className="navbar-logo">
           <img src="/logo-tecni-cuervo.jpeg" alt="Tecni Cuervo" />
           <span>Tecni Cuervo</span>
         </a>
